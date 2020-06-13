a329ffd0fb7d0398010dbba1d5d480d8e329f4e0
Edwin nduti <52169617+edwinnduti@users.noreply.github.com>
2020-06-13 18:06:48 +0300

    added body-parser middleware

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
-const app = express();
+const bodyParser = require('body-parser')
 
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 app.set("port",process.env.PORT || 5000);
 
 app.get("/",(req,res)=>{
