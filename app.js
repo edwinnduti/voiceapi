9ca06d7a89ec8da2e189b0224b3eee9c05a02472
Edwin nduti <52169617+edwinnduti@users.noreply.github.com>
2020-06-13 18:18:04 +0300

    added semicolons

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.set("port",process.env.PORT || 5000);
 
 app.get("/",(req,res)=>{
